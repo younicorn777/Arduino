b85e1386b8f8db0dd9fe47517868289f40dd26ab
younicorn777 <yhyun2002@naver.com>
2025-09-30 19:19:11 +0900

    Add files via upload

---
+++ MicroProcessor/miniProject4/miniProject4.ino
@@ -0,0 +1,77 @@
+/*
+// 실습 : 키패드로 값을 입력받기
+#include <Key.h>
+#include <Keypad.h>
+
+const byte ROWS = 4;
+const byte COLS = 4;
+char myKeys[ROWS][COLS] = {
+    {'0','1','2','3'},
+    {'4','5','6','7'},
+    {'8','9','A','B'},
+    {'C','D','E','F'},
+};
+
+byte rowPins[ROWS] = {5,4,3,2};
+byte colPins[COLS] = {8,9,10,11};
+Keypad customKeypad = Keypad(makeKeymap(myKeys), rowPins, colPins, ROWS, COLS);
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    char customKey = customKeypad.getKey();
+    if(customKey){
+        Serial.println(customKey);
+    }
+}
+*/
+
+// 미니프로젝트
+#include <Key.h>
+#include <Keypad.h>
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+char myKeys[ROWS][COLS] = {
+    {'0','1','2','3'},
+    {'4','5','6','7'},
+    {'8','9','A','B'},
+    {'C','D','E','F'}
+};
+byte rowPins[ROWS] = {5,4,3,2}; 
+byte colPins[COLS] = {8,9,10,11}; 
+Keypad customKeypad = Keypad(makeKeymap(myKeys), rowPins, colPins, ROWS, COLS);
+
+const String SECRET_PASSWORD = "1234"; 
+const size_t PASSWORD_LENGTH = SECRET_PASSWORD.length();
+
+String currentInput = "";
+
+void setup() {
+    Serial.begin(9600);
+    Serial.print("비밀번호를 입력하세요 : ");
+}
+
+void loop() {
+    char customKey = customKeypad.getKey();
+
+    if (customKey) {
+        Serial.print(customKey);
+        currentInput += customKey; 
+
+        if (currentInput.length() == PASSWORD_LENGTH) {
+
+            if (currentInput == SECRET_PASSWORD)
+                Serial.println("잠금 해제되었습니다"); 
+            else
+                Serial.println("비밀번호 오류입니다.");
+            
+            currentInput = "";
+            Serial.println("------------------------------------------");
+            Serial.print("비밀번호를 입력하세요 : ");
+        }
+    }
+    delay(50); 
+}
